be92a4d58ff6530a610b0085b1a3f0f20508ce37
Ludde Andersson <147702357+dradwul@user.noreply.github.com>
2023-11-27 11:30:10 +0100

    test test

--- Program.cs
+++ Program.cs
@@ -9,27 +9,32 @@ namespace HangmanGame
         private string secretWord;
         private int attemptsLeft;
         private List<char> guessedLetters;
+        private List<char> splitWord;
 
         public Hangman()
         {
             // Initialisera variabler och listor
             // Antal försök innan hängagubben ritas helt
             secretWord = PickRandomWord();
+            Console.WriteLine("The secret word is: " + secretWord);
+
         }
 
-        private string PickRandomWord()
+        public string PickRandomWord()
         {
             // Läs in ordlista från en textfil
-            string[] words = File.ReadAllLines("wordlist.txt");
+            string[] words = File.ReadAllLines("../../../wordlist.txt");
 
             // Välj ett slumpmässigt ord från listan
             Random rand = new Random();
-            return words[rand.Next(0, words.Length)].ToLower(); // Slumpmässigt ord
+            return words[rand.Next(0, words.Length)].ToLower(); // Slumpmässigt ord 
+            splitWord = new List<char>(secretWord.ToCharArray());
         }
 
         public void StartGame()
         {
             // Implementera start av spelet
+            Console.WriteLine("test test test");
         }
 
         public void DisplayHangman()
@@ -66,6 +71,30 @@ namespace HangmanGame
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "help")
+                {
+                    Console.WriteLine("helping you");
+                }
+                else if (input == "nej")
+                {
+                    Console.WriteLine("nej");
+                }
+                else if (input == "du")
+                {
+                    Console.WriteLine("du");
+                }
+                else if (input == "ja")
+                {
+                    Console.WriteLine("ja");
+                }
+                else if (input == "!!!!!!!!")
+                {
+                    Console.WriteLine("!!!!!!!");
+                }
+            }
             // Låt användaren gissa bokstäver och hantera spelets logik
             // Använd de olika funktionerna från Hangman-klassen
         }
